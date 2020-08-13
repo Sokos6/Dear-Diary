b88b465c3a8caa43843e6a3a6eaa3decfd0752d2
Sokos6 <will.sokolowski@gmail.com>
2020-08-12 21:12:08 -0500

    helper methods complete

--- src/services/mirage/routes/dairy.ts
+++ src/services/mirage/routes/dairy.ts
@@ -85,3 +85,30 @@ export const addEntry = (
     return handleErrors(error, 'Failed to save entry.');
   }
 };
+
+const getEntries = (
+  schema: any,
+  req: Request
+): { entries: Entry[] } | Response => {
+  try {
+    const diary = schema.diaries.find(req.params.id);
+    return diary.entry;
+  } catch (error) {
+    return handleErrors(error, 'Failed to get Diary entries');
+  }
+};
+
+export const updateEntry = (schema: any, req: Request): Entry | Response => {
+  try {
+    const entry = schema.entries.find(req.params.id);
+    const data = JSON.parse(req.requestBody) as Partial<Entry>;
+    const now = dayjs().format();
+    entry.update({
+      ...data,
+      updatedAt: now,
+    });
+    return entry.attrs as Entry;
+  } catch (error) {
+    return handleErrors(error, 'Failed to update entry.');
+  }
+};
